7f0f10a7cff51294cca1f04d6fb4d2e811469418
mudassiraliansari@gmail.com <googledell1994>
2020-06-05 23:15:08 +0500

    new structure

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-$("#send").click(function(){
-$.ajax({
-    type: "POST",
-    url: "send.php",
-    data: { "name": $("#name").val(), "email": $("#email").val(), "message": $("#message").val() },
-    success: function (response) {
-        console.log(response);
-        alert("Message Posted");
-        // window.location.href = "./";
-     },
-  });
+var mainEventClass = function () {
+  var main = this;
+  main.init = function () {
+    main.initEvents();
+  }
+  main.initEvents = function(){
+    main.bindNavBarClick();
+    main.bindFormSubmit();
+  }
+  main.bindNavBarClick = function(){
+    $(".page").click(function(){
+      $(".page").removeClass('active');
+      $(this).addClass('active');
+      if($(this).data('id')=='home')
+      {
+        $(".home").css('display','block')
+      }
+      else if($(this).data('id')=='about-me')
+      {
+        $(".about-me").css('display','block')
+      }
+      else if($(this).data('id')=='projects')
+      {
+        $(".projects").css('display','block')
+      }
+      else
+      {
+        $(".contact").css('display','block')
+      }
+    })
+  }; 
+  main.bindFormSubmit = function(){
+    $("#send").click(function(){
+      $.ajax({
+          type: "POST",
+          url: "send.php",
+          data: { "name": $("#name").val(), "email": $("#email").val(), "message": $("#message").val() },
+          success: function (response) {
+              alert("Message Posted");
+              window.location.href = "./";
+           }
+        })
+      })
+  };
+}
+$(function() {
+  var main = new mainEventClass();
+  main.init();
 });
